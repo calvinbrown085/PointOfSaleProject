5fe0f7a90a68d7c8882afcb462f90b6f502db78a
Calvin Brown <calvin@201-15.wireless.uni.edu>
2016-02-16 14:59:18 -0600

    Added files for our web request

---
+++ Database.py
@@ -0,0 +1,30 @@
+import json
+
+class MyDatabase():
+
+    def __init__(self):
+        jsonFile = open("items.json", "r").read()
+        jsonData = json.loads(jsonFile)
+        x = {}
+        y = {}
+        for item in jsonData["items"]:
+            x[str(item["id"])] = str(item["name"])
+            y[str(item["name"])] = str(item["id"])
+        self.items = x
+        self.itemsName = y
+
+
+    def getById(self,id):
+        try:
+            return self.items[id]
+        except KeyError:
+            return "no match"
+    def getByName(self,name):
+
+        for item in self.itemsName:
+            if item == name:
+                return item
+
+
+    def getItems(self):
+        return self.items
---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+from Database import MyDatabase
+import sqlite3
+db = MyDatabase()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+@app.route("/form", methods=["POST"])
+def form():
+    email = request.form["email"]
+    print(email)
+    return redirect("/")
+
+@app.route("/searchById", methods=["POST"])
+def searchById():
+    results = [db.getById(str(request.form["idSearch"]))]
+    return render_template("searchResults.html", results = results)
+
+@app.route("/searchByName", methods=["POST"])
+def searchByName():
+    results = [db.getByName(str(request.form["nameSearch"]))]
+    return render_template("searchResults.html", results = results)
+
+@app.route("/inventory")
+def inventory():
+    return render_template("inventory.html", items = db.getItems())
+
+if (__name__ == "__main__"):
+    app.run()
