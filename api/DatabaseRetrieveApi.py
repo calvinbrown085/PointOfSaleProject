fc578db5fa912d7d1d508d2ef38dd5cba527bced
Calvin Brown <calvin@216-235.wireless.uni.edu>
2016-02-18 16:17:16 -0600

    Added the design document for the database

--- api/DatabaseRetrieveApi.py
+++ api/DatabaseRetrieveApi.py
@@ -1,13 +1,22 @@
+from sqlalchemy import create_engine
+
+
+
+
 
 class DatabaseRetrieveApi():
     def __init__(self):
         #api stuff?
+        engine = create_engine('postgresql://localhost/[YOUR_DATABASE_NAME]')
 
     def getProductByName(self,productName):
         #my sql statement for getting product by name
+        return 0
 
     def getEmailList(self):
         #my sql statement for getting emails for the users
+        return 0
 
     def getProductById(self,productId):
         #my sql statement for getting a product by Id
+        return 0
