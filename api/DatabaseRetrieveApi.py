00818ff31468df391eb610bae3a2e33ca9e7bfea
Calvin Brown <calvin@bannos-MBP.hitronhub.home>
2016-02-17 19:37:24 -0600

    Removed copied code

--- api/DatabaseRetrieveApi.py
+++ api/DatabaseRetrieveApi.py
@@ -11,24 +11,3 @@ class DatabaseRetrieveApi():
 
     def getProductById(self,productId):
         #my sql statement for getting a product by Id
-
-import json
-
-class MyDatabase():
-
-    def __init__(self):
-        jsonFile = open("items.json", "r").read()
-        jsonData = json.loads(jsonFile)
-        x = {}
-        for item in jsonData["items"]:
-            x[str(item["id"])] = str(item["name"])
-        self.items = x
-
-    def getById(self,id):
-        try:
-            return self.items[id]
-        except KeyError:
-            return "no match"
-
-    def getItems(self):
-        return self.items
