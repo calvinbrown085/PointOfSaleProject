b998d01579f94ebe7dd042e563f107608fd5b150
Calvin Brown <calvin@bannos-MBP.hitronhub.home>
2016-03-08 19:32:29 -0600

    moved the db connection into the class

--- Database.py
+++ Database.py
@@ -1,13 +1,11 @@
 import json
 import os
 from DbConnection import DbConnection
-db = DbConnection()
-class Database():
 
+class Database():
+    
     def __init__(self):
-        x = {}
-        self.items = x
-        self.users = {}
+        self.db = DbConnection()
 
     def getById(self,id):
         try:
@@ -16,22 +14,22 @@ class Database():
             return "no match"
 
     def getItems(self):
-        return db.readQuery("""select * from inventory""")
+        return self.db.readQuery("""select * from inventory""")
 
     def getUser(self, username):
-        return db.readQuery("""select customer_name from customer_emails where customer_name = '{}'""".format(username))
+        return self.db.readQuery("""select customer_name from customer_emails where customer_name = '{}'""".format(username))
 
     def getEmailsInSystem(self):
-        return db.readQuery("""select * from customer_emails""")
+        return self.db.readQuery("""select * from customer_emails""")
 
     def writeEmailToDatabase(self, customerName, customerEmail):
-        db.writeQuery("""insert into customer_emails values ('{}','{}')""".format(customerName,customerEmail))
+        self.db.writeQuery("""insert into customer_emails values ('{}','{}')""".format(customerName,customerEmail))
 
     def insertNewInventoryItem(self, name, productId, originalCost, sellPrice, supplier, productType, amountInStock):
-        db.writeQuery("""insert into inventory values ('{}','{}','{}','{}','{}','{}','{}')""".format(name,productId, originalCost, sellPrice, supplier, productType, amountInStock))
+        self.db.writeQuery("""insert into inventory values ('{}','{}','{}','{}','{}','{}','{}')""".format(name,productId, originalCost, sellPrice, supplier, productType, amountInStock))
 
     def insertIntoInventoryLog(self,productId, productsRecieved, productsSold):
-        db.writeQuery("""insert into inventory_log values ('{}','{}', '{}')""".format(productId, productsRecieved, productsSold))
+        self.db.writeQuery("""insert into inventory_log values ('{}','{}', '{}')""".format(productId, productsRecieved, productsSold))
 
     def insertNewManagerCredentials(self,username, password):
-        db.writeQuery("""insert into manager_credentials values ('{}','{}')""".format(username, password))
+        self.db.writeQuery("""insert into manager_credentials values ('{}','{}')""".format(username, password))
